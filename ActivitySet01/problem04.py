4770498ebb44476e5a2d62af8f197a62d4a91e1f
Pooja <96231234+poojas2003@users.noreply.github.com>
2022-05-23 04:58:11 +0000

    activity 4

--- ActivitySet01/problem04.py
+++ ActivitySet01/problem04.py
@@ -1,3 +1,13 @@
 # Conditional Execution
 
 hrs = input("Enter hours? ")
+h=float(hrs)
+rate=input("Enter rate?")
+r=float(rate)
+if h>40 :
+  lmn=40*r
+  pqr=1.5*(h-40)*r
+  xp=lmn+pqr
+else:
+  xp=h*r
+print(xp)  
--- ActivitySet01/problem14.py
+++ ActivitySet01/problem14.py
@@ -1,2 +1,4 @@
 # Using Web Services
 # https://www.py4e.com/lessons/servces
+x=5
+print(x)
